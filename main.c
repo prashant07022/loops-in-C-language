fd9829fb3f489b010b848929cfb34895996fc1b1
Prashant <prashant07022@gmail.com>
2026-07-04 12:04:35 +0530

    Add for loop example in main.c
    Add a simple for loop example in C to print numbers 1 to 5.

---
+++ main.c
@@ -0,0 +1,21 @@
+//We study loops in C language like (for, while, do-while)
+#include <stdio.h>
+
+int main() {
+    
+    for (int i = 1; i <= 5; i++) 
+    
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
+
+## Output
+
+    1
+    2
+    3
+    4
+    5
