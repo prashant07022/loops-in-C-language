affbb5e3302be9d028a39c14863cb487d5d67b63
Prashant <prashant07022@gmail.com>
2026-07-04 12:20:01 +0530

    Add loop examples in main.c
    Updated main.c to include for, while, and do-while loop examples.

--- main.c
+++ main.c
@@ -1,13 +1,40 @@
 //We study loops in C language like (for, while, do-while)
+//for loop basic program (print 1 to 5 number)
 #include <stdio.h>
-
-int main() {
-    
+int main() 
+{
+    // A basic for loop to print numbers from 1 to 5
     for (int i = 1; i <= 5; i++) 
-    
     {
         printf("%d\n", i);
     }
-    
+    return 0;
+}
+
+//While loop basic program (print 1 to 5 number)
+#include <stdio.h>
+int main() 
+{
+    // 1. Initialization: Set up the starting value
+    int i = 1; 
+    // 2. Condition: The loop runs as long as this remains true
+    while (i <= 5)
+    {
+        printf("Number: %d\n", i);
+        // 3. Updation: Change the variable so the loop eventually ends
+        i++; 
+    }
+    return 0;
+}
+
+//Do-While loop basic program (print 1 to 5 number)
+#include <stdio.h>
+int main() 
+{
+    int i = 1; // Initialization
+    do {
+        printf("Number: %d\n", i);
+        i++; // Increment counter
+        } while (i <= 5); // Condition check
     return 0;
 }
